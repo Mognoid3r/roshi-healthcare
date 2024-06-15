24f9b52b21bde35e802cfbcecf64e51493eb6aa0
mognoid3r <allon.paz@gmail.com>
2024-06-15 14:31:51 -0700

    testing deploy

--- backend/models/Exercise.js
+++ backend/models/Exercise.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const exerciseSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  reps: {
-    type: Number,
-    required: true,
-  },
-  sets: {
-    type: Number,
-    required: true,
-  },
-});
+// const exerciseSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   reps: {
+//     type: Number,
+//     required: true,
+//   },
+//   sets: {
+//     type: Number,
+//     required: true,
+//   },
+// });
 
-module.exports = mongoose.model("Exercise", exerciseSchema);
+// module.exports = mongoose.model("Exercise", exerciseSchema);
--- src/components/PendingRequestButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PendingRequestButton = () => {
-  return <button disabled>Pending Request</button>;
-};
-
-export default PendingRequestButton;
